129d37df981f97433c81bede97466e8b8e6b722b
liemdev <buithanhliem5073@gmail.com>
2026-02-04 16:57:23 +0700

    update code

--- src/components/list-tweets/ItemTweet.tsx
+++ src/components/list-tweets/ItemTweet.tsx
@@ -109,9 +109,6 @@ export const SkeletonTweet = ({ count = 1 }: { count?: number }) => {
               <div className="h-3 bg-gray-200 rounded w-24"></div>
             </div>
           </div>
-          <div className="h-4 bg-gray-200 rounded w-full my-2"></div>
-          <div className="h-4 bg-gray-200 rounded w-3/4 mb-3"></div>
-          <div className="h-3 bg-gray-200 rounded w-3/4 my-3"></div>
           <div className="w-full aspect-video bg-gray-200 rounded-lg mb-4"></div>
           <div className="flex space-x-6">
             <div className="h-3 bg-gray-200 rounded w-12"></div>
